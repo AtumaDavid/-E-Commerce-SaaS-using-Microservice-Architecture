da544836f62812e0c7a72e63b2f3063a42774f7c
Atuma David <atumadavid35@gmail.com>
2025-04-17 11:30:47 +0100

    global error handler

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,5 +1,7 @@
+import { errorMiddleware } from './../../../packages/error-handler/error-middleware';
 import express from 'express';
 import cors from 'cors'
+import cookieParser from 'cookie-parser';
 
 // const host = process.env.HOST ?? 'localhost';
 // const port = process.env.PORT ? Number(process.env.PORT) : 6001;
@@ -11,10 +13,14 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "X-Requested-With", "Authorization"],
     credentials: true,
 }))
+app.use(express.json());
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.send({ 'message': 'Hello API' });
 });
+
+app.use(errorMiddleware)
 
 const port = process.env.PORT || 6001;
 const server = app.listen(port, () => {
